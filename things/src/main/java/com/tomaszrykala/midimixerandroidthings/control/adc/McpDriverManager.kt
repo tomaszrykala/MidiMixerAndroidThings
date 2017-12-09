2c2e53e71b79330c11f32554bed72a768ad8864f
tomaszrykala <tomasz.a.rykala@gmail.com>
2017-12-09 09:39:11 +0000

    correct mcp clock pin mapping

--- things/src/main/java/com/tomaszrykala/midimixerandroidthings/control/adc/McpDriverManager.kt
+++ things/src/main/java/com/tomaszrykala/midimixerandroidthings/control/adc/McpDriverManager.kt
@@ -9,13 +9,13 @@ class McpDriverManager(private val presenter: MidiControllerContract.Presenter) 
         val mixerAdcStartChannel = 10
     }
 
-    //    private val fxController: McpDriver = McpDriver("BCM8", "BCM8", "BCM10", "BCM9")
-    private val mixerMcpDriver: McpDriver = McpDriver("BCM8", "BCM8", "BCM10", "BCM9")
+    //    private val fxController: McpDriver = McpDriver("BCM8", "BCM11", "BCM10", "BCM9")
+    private val mixerMcpDriver: McpDriver = McpDriver("BCM8", "BCM11", "BCM10", "BCM9")
     private val mixerMidiPots: MutableList<MidiPot> = mutableListOf()
 
     fun start() {
         mixerMcpDriver.start()
-        (0..5).mapTo(mixerMidiPots) {
+        (0..6).mapTo(mixerMidiPots) {
             MidiPot(mixerMcpDriver, presenter, it, (mixerAdcStartChannel + it).toByte()).apply { start() }
         }
     }
